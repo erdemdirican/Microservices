771ae272086882fa3a973c2875aec7da8103f51c
Erdem Dirican <erdemdirican@hotmail.com>
2022-04-02 15:42:46 +0300

    Added Discount API.

--- Services/Discount/FreeCourse.Services.Discount/Startup.cs
+++ Services/Discount/FreeCourse.Services.Discount/Startup.cs
@@ -1,5 +1,4 @@
-using FreeCourse.Services.Basket.Services;
-using FreeCourse.Services.Basket.Settings;
+using FreeCourse.Services.Discount.Services;
 using FreeCourse.Shared.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
@@ -11,7 +10,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
@@ -19,7 +17,7 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace FreeCourse.Services.Basket
+namespace FreeCourse.Services.Discount
 {
     public class Startup
     {
@@ -33,27 +31,17 @@ namespace FreeCourse.Services.Basket
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHttpContextAccessor();
+            services.AddScoped<ISharedIdentityService, SharedIdentityService>();
+            services.AddScoped<IDiscountService, DiscountService>();
+
             var requireAuthorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
             JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
             {
                 options.Authority = Configuration["IdentityServerURL"];
-                options.Audience = "resource_basket";
+                options.Audience = "resource_discount";
                 options.RequireHttpsMetadata = false;
-            });
-
-            services.AddHttpContextAccessor();
-            services.AddScoped<ISharedIdentityService, SharedIdentityService>();
-            services.AddScoped<IBasketService, BasketService>();
-
-            services.Configure<RedisSettings>(Configuration.GetSection("RedisSettings"));
-
-            services.AddSingleton<RedisService>(sp =>
-            {
-                var redisSettings = sp.GetRequiredService<IOptions<RedisSettings>>().Value;
-                var redis = new RedisService(redisSettings.Host, redisSettings.Port);
-                redis.Connect();
-                return redis;
             });
 
             services.AddControllers(opt =>
@@ -62,7 +50,7 @@ namespace FreeCourse.Services.Basket
             });
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "FreeCourse.Services.Basket", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "FreeCourse.Services.Discount", Version = "v1" });
             });
         }
 
@@ -73,7 +61,7 @@ namespace FreeCourse.Services.Basket
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "FreeCourse.Services.Basket v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "FreeCourse.Services.Discount v1"));
             }
 
             app.UseRouting();
